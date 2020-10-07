087a751adb94b0ca27b4f228a16fc2eb81f1bfd8
Rodry-12 <jimenezrodry12@gmail.com>
2020-10-07 08:16:15 -0500

    Las Entidades establecidas de la base de datos

---
+++ src/main/java/com/pascualbravol/cleancarweb/models/entity/Vehiculos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pascualbravol.cleancarweb.models.entity;
+
+/**
+ *
+ * @author acer
+ */
+public class Vehiculos {
+    
+}
