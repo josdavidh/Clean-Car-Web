a729d8c967d7c27225cc1ffda039b64045ad5ef2
Jose <davidherrera0429@gmail.com>
2020-10-07 07:20:12 -0500

    Nuestro primer commit, inicio de la aplicacion base con paquetes creados

---
+++ src/main/java/com/pascualbravo/cleancarweb/CleanCarWebApplication.java
@@ -0,0 +1,11 @@
+package com.pascualbravo.cleancarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCarWebApplication {
+    public static void main(String [] args){
+        SpringApplication.run(CleanCarWebApplication.class, args);
+    }
+}
