63df49c75974f615114c9611eddc539a9ded18a9
Jose <davidherrera0429@gmail.com>
2020-10-08 13:42:53 -0500

    clase perfil lista

--- src/main/java/com/pascualbravol/cleancarweb/models/entity/Perfil.java
+++ src/main/java/com/pascualbravol/cleancarweb/models/entity/Perfil.java
@@ -1,14 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pascualbravol.cleancarweb.models.entity;
 
-/**
- *
- * @author acer
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Perfil")
 public class Perfil {
+    @Id
+    @Column(name = "Id_Perfil")
+    private int idPerfil;
+    
+    @Column(name = "Usuario")
+    private String usuario;
+    
+    @Column(name = "Password")
+    private String password;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Cedula_Admin")
+    private Administradores cedulaAdmin;
+
+    /**
+     * @return the idPerfil
+     */
+    public int getIdPerfil() {
+        return idPerfil;
+    }
+
+    /**
+     * @param idPerfil the idPerfil to set
+     */
+    public void setIdPerfil(int idPerfil) {
+        this.idPerfil = idPerfil;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the cedulaAdmin
+     */
+    public Administradores getCedulaAdmin() {
+        return cedulaAdmin;
+    }
+
+    /**
+     * @param cedulaAdmin the cedulaAdmin to set
+     */
+    public void setCedulaAdmin(Administradores cedulaAdmin) {
+        this.cedulaAdmin = cedulaAdmin;
+    }
+    
     
 }
