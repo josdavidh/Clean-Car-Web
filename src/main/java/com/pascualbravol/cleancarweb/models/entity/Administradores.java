69e0e4494e54776c4a8d95b7b85fec9ec176d3e9
Jose <davidherrera0429@gmail.com>
2020-10-08 12:34:53 -0500

    Models entity Administradores creada

--- src/main/java/com/pascualbravol/cleancarweb/models/entity/Administradores.java
+++ src/main/java/com/pascualbravol/cleancarweb/models/entity/Administradores.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pascualbravol.cleancarweb.models.entity;
 
-/**
- *
- * @author acer
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name = "Administradores")
 public class Administradores {
+    @Id
+    @Column(name = "Cedula_Admin")
+    private int cedulaAdmin;
+    
+    @Column(name = "Nombres")
+    private String nombres;
+    
+    @Column(name = "Apellidos")
+    private String apellidos;
+
+    /**
+     * @return the cedulaAdmin
+     */
+    public int getCedulaAdmin() {
+        return cedulaAdmin;
+    }
+
+    /**
+     * @param cedulaAdmin the cedulaAdmin to set
+     */
+    public void setCedulaAdmin(int cedulaAdmin) {
+        this.cedulaAdmin = cedulaAdmin;
+    }
+
+    /**
+     * @return the nombres
+     */
+    public String getNombres() {
+        return nombres;
+    }
+
+    /**
+     * @param nombres the nombres to set
+     */
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
     
 }
